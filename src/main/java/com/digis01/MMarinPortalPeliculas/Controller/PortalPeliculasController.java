2f9644279ed4bf84950062036ba616e06be65956
CesarFloresGutierrez <chesaresuport@gmail.com>
2025-05-19 15:12:03 -0600

    se completo validacion de login

--- src/main/java/com/digis01/MMarinPortalPeliculas/Controller/PortalPeliculasController.java
+++ src/main/java/com/digis01/MMarinPortalPeliculas/Controller/PortalPeliculasController.java
@@ -1,18 +1,43 @@
-
 package com.digis01.MMarinPortalPeliculas.Controller;
 
+
+import com.digis01.MMarinPortalPeliculas.Model.Auth;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("/AficionPelis")
 public class PortalPeliculasController {
+
+    private String ApiKey = "05452f9f66341a2b27f7389fa8ba94cb";
+    private String BaseUrl = "https://api.themoviedb.org/3/";
+    private RestTemplate restTemplate;
     
     @GetMapping()
-    public String Login(){
-        System.out.println("s");
+    public String Login(Model model) {
+        Auth auth = new Auth();
+        
+        model.addAttribute("auth", auth);
         return "Login";
     }
-    
+    @PostMapping("Login")
+    public String CreaSession(@ModelAttribute Auth auth, Model model){
+        try {
+            if((!auth.getUserName().isEmpty())&&(!auth.getPassword().isEmpty()) ){
+                
+                return "";
+            }else{
+                model.addAttribute("error", "Error datos vacios");
+                return "Login";
+            }
+            
+        } catch (Exception e) {
+        }        
+            return "";
+    }
 }
---
+++ src/main/java/com/digis01/MMarinPortalPeliculas/Model/Auth.java
@@ -0,0 +1,27 @@
+
+package com.digis01.MMarinPortalPeliculas.Model;
+
+
+public class Auth {
+    private String UserName;
+    private String Password;
+ 
+    public void setUserName(String UserName){
+        this.UserName = UserName;
+        
+    }
+    public String getUserName(){
+        return this.UserName;        
+    }
+    public void setPassword(String Password){
+        this.Password = Password;
+    }
+    public String getPassword(){
+        return this.Password;
+    }
+
+  
+  
+    
+    
+}
